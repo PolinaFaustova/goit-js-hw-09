4c1d0abe4a52d2f2af65be334d0cc8f46688f8dc
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-16 18:30:14 +0100

    start 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const handleClickStart = () => {
+  getRandomHexColor();
+};
+const handleClickStop = () => {
+  console.log('Button was clicked');
+};
+
+refs.buttonStart.addEventListener('click', handleClickStart);
+refs.buttonStop.addEventListener('click', handleClickStop);
