a3ef0119f5b6bc59c7e6207524151e300938d500
PolinaFaustova <115539477+PolinaFaustova@users.noreply.github.com>
2023-03-16 18:45:00 +0000

    Deploying to gh-pages from @ PolinaFaustova/goit-js-hw-09@1c192f3651f7dcc2e021921e30511eca39667f9f 🚀

--- 01-color-switcher.1a55a507.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]")};t.buttonStart.addEventListener("click",(function(){"#".concat(Math.floor(16777215*Math.random()).toString(16))})),t.buttonStop.addEventListener("click",(function(){console.log("Button was clicked")}))}();
-//# sourceMappingURL=01-color-switcher.1a55a507.js.map
