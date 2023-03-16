1c192f3651f7dcc2e021921e30511eca39667f9f
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-16 19:44:32 +0100

    first task ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,26 @@
 const refs = {
-  buttonStart: document.querySelector('button[data-start]'),
-  buttonStop: document.querySelector('button[data-stop]'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
 };
+let timerId = null;
+
+refs.startBtn.addEventListener('click', event => {
+  if (!refs.startBtn.classList.contains('disabled')) {
+    refs.startBtn.classList.add('disabled');
+    refs.stopBtn.classList.remove('disabled');
+  }
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+refs.stopBtn.addEventListener('click', event => {
+  clearInterval(timerId);
+  refs.startBtn.classList.remove('disabled');
+  if (!refs.stopBtn.classList.contains('disabled')) {
+    refs.stopBtn.classList.add('disabled');
+  }
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const handleClickStart = () => {
-  getRandomHexColor();
-};
-const handleClickStop = () => {
-  console.log('Button was clicked');
-};
-
-refs.buttonStart.addEventListener('click', handleClickStart);
-refs.buttonStop.addEventListener('click', handleClickStop);
