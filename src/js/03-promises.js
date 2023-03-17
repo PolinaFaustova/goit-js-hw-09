1cfb628a6a76446d9619775cedff6ba9bf4c7c5e
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-17 15:45:26 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+const refs = {
+  form: document.querySelector('.form'),
+  delay: form.querySelector('input[name="delay"]'),
+  step: form.querySelector('input[name="step"]'),
+  amount: form.querySelector('input[name="amount"]'),
+  btnSubmit: document.querySelector('button[type="submit"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
