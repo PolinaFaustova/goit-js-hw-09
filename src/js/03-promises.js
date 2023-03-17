31da5b8c8ee8dd0c22f6ae2f28b00965edbe82a8
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-17 18:12:43 +0100

    new update 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,9 @@
+const form = document.querySelector('.form');
 const refs = {
-  form: document.querySelector('.form'),
-  delay: form.querySelector('input[name="delay"]'),
-  step: form.querySelector('input[name="step"]'),
-  amount: form.querySelector('input[name="amount"]'),
+  form,
+  delayInput: form.querySelector('input[name="delay"]'),
+  stepInput: form.querySelector('input[name="step"]'),
+  amountInput: form.querySelector('input[name="amount"]'),
   btnSubmit: document.querySelector('button[type="submit"]'),
 };
 
@@ -10,20 +11,22 @@ refs.form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-}
 
+  createPromise(position, delay);
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  delay;
   if (shouldResolve) {
     // Fulfill
-    createPromise.then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    });
   } else {
     // Reject
-    createPromise.catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
