7a2756b168a73a91f148cedff1ff9eeff4fe87cd
PolinaFaustova <115539477+PolinaFaustova@users.noreply.github.com>
2023-03-17 14:45:55 +0000

    Deploying to gh-pages from @ PolinaFaustova/goit-js-hw-09@1cfb628a6a76446d9619775cedff6ba9bf4c7c5e 🚀

---
+++ 03-promises.fa8eb6d7.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),form.querySelector('input[name="delay"]'),form.querySelector('input[name="step"]'),form.querySelector('input[name="amount"]'),document.querySelector('button[type="submit"]');
+//# sourceMappingURL=03-promises.fa8eb6d7.js.map
